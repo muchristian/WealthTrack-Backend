58ed4e6c5f787050009f53a1f4a799bf20c1d8ec
muchris <mucyochristian2@gmail.com>
2022-11-10 16:14:29 +0200

    configure database url for production

--- config/database.ts
+++ config/database.ts
@@ -1,14 +1,15 @@
 const path = require("path");
-
+const parseDbUrl = require("parse-database-url");
+const dbConfig = parseDbUrl(process.env.DATABASE_URL);
 export = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+      host: env("DATABASE_HOST") || dbConfig.host,
+      port: env.int("DATABASE_PORT") || dbConfig.port,
+      database: env("DATABASE_NAME") || dbConfig.database,
+      user: env("DATABASE_USERNAME") || dbConfig.user,
+      password: env("DATABASE_PASSWORD") || dbConfig.password,
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
